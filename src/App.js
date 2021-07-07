f1f81b369a2b3f31c9f4ac276ff18509b6181735
Shibumishra <sibumishra487@gmail.com>
2021-07-07 09:19:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Topbar from './components/Topbar';
+import Home from './components/Home';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart';
+import OrderConform from './components/Order';
+import SignInOutContainer from './TabsContainers';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Topbar />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/product/:id"  component={ProductDetails} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/container" component={SignInOutContainer} />
+      <Route path="/order" component={OrderConform} />
+    </Switch>
+    </BrowserRouter>
+ 
   );
 }
 
