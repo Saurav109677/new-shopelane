736e14b8b5c415bb2b0fb63032bd9cf0f4360ab3
Saurav109677 <34793927+Saurav109677@users.noreply.github.com>
2022-01-25 13:15:02 +0530

    add product api for goLakshmi

--- src/Ultis/Apis/index.js
+++ src/Ultis/Apis/index.js
@@ -1,6 +1,7 @@
 //Slider Api
 export const sliderAPI = "https://60cce86871b73400171f8b70.mockapi.io/slider";
 //Homepage API
-export const homeProductsAPI = "https://5d76bf96515d1a0014085cf9.mockapi.io/product";
+//export const homeProductsAPI = "https://5d76bf96515d1a0014085cf9.mockapi.io/product";
+export const homeProductsAPI = "https://golakshmi.herokuapp.com/api/v1/product/showAll";
 
 export const getProduct = 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/';
